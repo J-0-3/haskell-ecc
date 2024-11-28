fca13940f47db25cd4b7b4e0682c4a9d65cc80b0
j-0-3 <38348883+J-0-3@users.noreply.github.com>
2024-11-28 00:30:50 +0000

    feat: adds double-and-add point multiplication, fix: prevents addPoints gradient becoming negative

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,11 @@
 module Main (main) where
 
-import Lib (doublePoint, CurvePoint(..))
+import Lib (doublePoint, CurvePoint(..), multiplyPoint)
 
 main :: IO ()
 main = do
   let c = (497, 1768, 9739)
+  let multiplyPoint' = multiplyPoint c
   let doublePoint' = doublePoint c
 
-  print $ doublePoint' $ Point 5274 2841
+  print $ multiplyPoint' (Point 5323 5438) 1337
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,4 @@
-module Lib (doublePoint, addPoints, CurvePoint(Point, Infinity), modInverse) where
+module Lib (doublePoint, addPoints, CurvePoint (Point, Infinity), modInverse, multiplyPoint) where
 
 type WeierstrassCurve = (Integer, Integer, Integer)
 
@@ -9,7 +9,10 @@ modInverse' _ t2 1 _ = t2
 modInverse' t1 t2 x m = modInverse' t2 (t1 - (m `div` x) * t2) (m `mod` x) x
 
 modInverse :: Integer -> Integer -> Integer
-modInverse x m = mod (modInverse' 0 1 x m) m
+modInverse 0 _ = error "Cannot compute inverse 0 (undefined)"
+modInverse x m
+  | gcd x m == 1 = mod (modInverse' 0 1 x m) m
+  | otherwise = error "Cannot compute inverse over divisible modulus"
 
 tangentGradAtPoint :: WeierstrassCurve -> CurvePoint -> Integer
 tangentGradAtPoint (a, _, m) (Point x y) = ((3 * (x ^ 2) + a) * modInverse (2 * y) m) `mod` m
@@ -28,8 +31,15 @@ addPoints (a, b, m) (Point x y) (Point x' y')
   | (x, y) == (x', -y') = Infinity
   | otherwise = Point x'' ((λ * x - λ * x'' - y) `mod` m)
   where
-    λ = ((y' - y) * modInverse (x' - x) m) `mod` m
+    λ = (((y' - y) `mod` m) * modInverse ((x' - x) `mod` m) m) `mod` m
     x'' = (λ ^ 2 - x - x') `mod` m
 addPoints _ (Point x y) Infinity = Point x y
 addPoints _ Infinity (Point x y) = Point x y
 addPoints _ Infinity Infinity = Infinity
+
+multiplyPoint' :: CurvePoint -> WeierstrassCurve -> CurvePoint -> Integer -> CurvePoint
+multiplyPoint' r _ _ 0 = r
+multiplyPoint' r c q n = multiplyPoint' (if (n `mod` 2) == 1 then addPoints c r q else r) c (doublePoint c q) (n `div` 2)
+
+multiplyPoint :: WeierstrassCurve -> CurvePoint -> Integer -> CurvePoint
+multiplyPoint = multiplyPoint' Infinity
